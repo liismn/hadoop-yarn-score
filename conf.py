a021ed684d0181b313764aa2d360fe2a9acba784
ubuntu <ubuntu@vm-1476374481482.vm-14763744814821476374481482>
2017-12-08 15:19:40 +0800

    fix bug in conf.py for total memory

--- conf.py
+++ conf.py
@@ -67,10 +67,10 @@ class Config(metaclass=Singleton):
         self.stat_interval = interval
 
     def get_sys_total_memory(self):
-        return self.sys_total_memory
+        return self.total_sys_memory
 
     def set_sys_total_memory(self, size):
-        self.sys_total_memory = size
+        self.total_sys_memory = size
 
     def get_rest_port(self):
         return self.rest_port
